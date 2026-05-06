d8c16c2be1bb838688d3cd029cc77fd5cb518b98
Stefan Traub <Stefan.Traub@thu.de>
2026-05-06 10:44:13 +0200

    check if file exists

--- webserver/Program.cs
+++ webserver/Program.cs
@@ -9,6 +9,8 @@ using System.Security.Cryptography;
 
 class MiniWebServer
 {
+    const string WWW_ROOT = "wwwroot";
+    const string HOME_PAGE = "index.html";
     static void Main(string[] args)
     {
         IPEndPoint ep = new IPEndPoint(IPAddress.Any, 9000);
@@ -26,9 +28,19 @@ class MiniWebServer
             int n = cl.Receive(buf);
             string request = Encoding.UTF8.GetString(buf, 0, n);
             string[] parts = request.Split(" ");
-            Console.WriteLine(parts[1]);
+            string filename = parts[1].Substring(1);
+            if (filename == "") filename = HOME_PAGE;
+            filename = Path.Combine(WWW_ROOT, filename);
+            Console.WriteLine(filename);
             string mime = "text/html";
-            string filename = "index.html";
+
+            if (!File.Exists(filename)) {
+                string headerne = "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n<h1>File not found</h1>\r\n";
+                cl.Send(Encoding.UTF8.GetBytes(headerne));
+                cl.Close();
+                continue;
+            }
+
             byte[] content = File.ReadAllBytes(filename);
             string header = $"HTTP/1.0 200 OK\r\nContent-Type: {mime}\r\nContent-Length: {content.Length}\r\n\r\n";
             cl.Send(Encoding.UTF8.GetBytes(header));
