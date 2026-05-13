f253296b61214db5ecc7c854978ee62c54b64530
Stefan Traub <Stefan.Traub@thu.de>
2026-05-13 10:34:21 +0200

    person list

--- project/Pages/Index.cshtml.cs
+++ project/Pages/Index.cshtml.cs
@@ -5,15 +5,14 @@ namespace project.Pages;
 
 public class IndexModel : PageModel
 {
-    public Person person { get; set; }
+    public List<Person> plist { get; set; }
     public IndexModel()
     {
-        person = new Person
+        plist = new List<Person>
         {
-            Id = 1,
-            FirstName = "John",
-            LastName = "Doe",
-            Age = 30
+            new Person { Id = 1, FirstName = "John", LastName = "Doe", Age = 30 },
+            new Person { Id = 2, FirstName = "Jane", LastName = "Smith", Age = 25 },
+            new Person { Id = 3, FirstName = "Bob", LastName = "Johnson", Age = 40 }
         };
     }
     public void OnGet()
