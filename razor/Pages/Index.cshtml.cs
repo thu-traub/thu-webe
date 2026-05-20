1fb67f0cd8cb75e8556f885becd3e5d029e71f8f
Stefan Traub <Stefan.Traub@thu.de>
2026-05-20 09:33:06 +0200

    hidden field

--- razor/Pages/Index.cshtml.cs
+++ razor/Pages/Index.cshtml.cs
@@ -6,7 +6,11 @@ namespace razor.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> logger;
-    public string Message { get; set; } = "";
+    public string Message { get; set; }
+    [BindProperty] public int cnt { get; set; }
+
+    // [BindProperty]
+    // public Test test { get; set; }
     private readonly ITime timeService;
 
     public IndexModel(ILogger<IndexModel> logger, ITime timeService)
@@ -14,9 +18,31 @@ public class IndexModel : PageModel
         this.logger = logger;
         this.timeService = timeService;
     }
-    public void OnGet()
+    public void OnGet(string id)
     {
-        logger.LogInformation("Index page visited at {Time}", DateTime.Now);
-        Message = timeService.GetTime();
+        Message = cnt.ToString();
     }
+
+    public void OnPost()
+    {
+        Message = "#click = " + (++cnt);
+        // string? c = HttpContext.Request.Cookies["cnt"];
+        // if (c != null) cnt = Int32.Parse(c);
+        // Message = "#click = " + (++cnt);
+        // HttpContext.Response.Cookies.Append("cnt", cnt.ToString());
+    }
+    // public void OnPost(string button)
+    // {
+    //     logger.LogInformation($"Button {button} clicked at {DateTime.Now}");
+    // }
+    // public void OnPostEdit(string button)
+    // {
+    //     logger.LogInformation($"Edit: Button {button} clicked at {DateTime.Now}");
+    // }
 }
+
+// public class Test
+// {
+//     public string name { get; set; } = "";
+//     public string alter { get; set; } = "";
+// }
