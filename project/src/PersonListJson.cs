dc341649fc2b801d5314cdd2612bf205f977b22a
Stefan Traub <Stefan.Traub@thu.de>
2026-05-20 10:52:32 +0200

    Json Persistence

---
+++ project/src/PersonListJson.cs
@@ -0,0 +1,62 @@
+using System.Text.Json;
+
+public class PersonListJson : IPersonConnector
+{
+    const string FILEPATH = "data/personlist.json"; 
+    List<Person> plist = new List<Person>();
+    public PersonListJson()
+    {
+        if (File.Exists(FILEPATH))
+        {
+            string json = File.ReadAllText(FILEPATH);
+            plist = JsonSerializer.Deserialize<List<Person>>(json) ?? new List<Person>();
+        } else
+        {
+            plist.Add(new Person { Id = 1, FirstName = "John", LastName = "Doe", Age = 30 });
+            plist.Add(new Person { Id = 2, FirstName = "Jane", LastName = "Smith", Age = 25 });
+            plist.Add(new Person { Id = 3, FirstName = "Bob", Age = 40 });
+            Save();
+        }
+    }
+    void Save()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+        string json = JsonSerializer.Serialize(plist, options);
+        File.WriteAllText(FILEPATH, json);
+    }
+    public Person Create(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Person>? Get()
+    {
+        return plist;
+    }
+
+    public Person? Get(int id)
+    {
+        return plist.FirstOrDefault(p => p.Id == id);
+    }
+
+    public bool Update(Person person)
+    {
+        Person? old = plist.FirstOrDefault(p => p.Id == person.Id);
+        if (old != null)
+        {
+            old.FirstName = person.FirstName;
+            old.LastName = person.LastName;
+            old.Age = person.Age;
+            Save();
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+}
--- project/src/Program.cs
+++ project/src/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<IPersonConnector, PersonListMemory>();
+builder.Services.AddSingleton<IPersonConnector, PersonListJson>();
 
 var app = builder.Build();
 
