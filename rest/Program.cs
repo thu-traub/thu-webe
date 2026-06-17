dce594b07a84a797340884f863e6e935360da5ed
Stefan Traub <Stefan.Traub@thu.de>
2026-06-17 09:29:33 +0200

    rest api 2

--- rest/Program.cs
+++ rest/Program.cs
@@ -5,16 +5,38 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+List<Person> DemoPerson = new List<Person> { 
+    new Person { Id=1,Name = "John" },
+    new Person { Id=2, Name = "Jane" }
+};
+
 app.MapOpenApi();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/api/hello", () => new Person { Name = "World" });
+app.MapGet("/api/person", () => DemoPerson);
+app.MapGet("/api/person/{id}", (int id) => DemoPerson.FirstOrDefault(p => p.Id == id));
+app.MapPost("/api/person", (Person person) =>
+{
+    DemoPerson.Add(person);
+    return Results.Ok(person);
+}); 
+app.MapPut("/api/person/{id}", (int id, Person updatedPerson) =>
+{
+    var person = DemoPerson.FirstOrDefault(p => p.Id == id);
+    if (person is null)
+    {
+        return Results.NotFound();
+    }
+    person.Name = updatedPerson.Name;
+    return Results.Ok(person);
+});
 
 app.Run();
 
 public class Person
 {
+    public int Id { get; set; }
     public required string Name { get; set; }
 }
